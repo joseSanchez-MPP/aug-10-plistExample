e2688f58d7779333ac9ea0a331c818e5160237ed
Jose Sanchez <josersanchez0117@gmail.com>
2020-08-10 13:40:34 -0500

    A

--- PListDataExample/ViewController.swift
+++ PListDataExample/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var TextBox: UITextField!
+    @IBOutlet var mainSwitch: UISwitch!
+    @IBOutlet var mainSlider: UISlider!
+    
+    @IBAction func onSaveButtonPressed(_ sender: Any) {
+        UserDefaults.standard.set(TextBox.text, forKey: "textBox")
+        UserDefaults.standard.set(mainSlider.value, forKey: "mainSlider")
+        UserDefaults.standard.set(mainSwitch.isOn, forKey: "mainSwitch")
+    }
+    
+    @IBAction func onLoadButtonPressed(_ sender: Any) {
+        TextBox.text = UserDefaults.standard.string(forKey: "textBox")
+        mainSlider.value = UserDefaults.standard.float(forKey: "mainSlider")
+        mainSwitch.isOn = UserDefaults.standard.bool(forKey: "mainSwitch")
+    }
+    
+    @IBAction func onDeleteButtonPressed(_ sender: Any) {
+        mainSwitch.isOn = true
+        mainSlider.value = 0.5
+        TextBox.text = "Hello World!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
